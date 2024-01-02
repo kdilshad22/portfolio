9875f12239a835ead9bab999a4798ff5414a5e02
kochar <kochard96@gmail.com>
2024-01-02 18:37:11 +0300

    change myresume

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Front-End Developer 🚀 having an experience of building Web with JavaScript / Reactjs / Nodejs / and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1m5_UZOeaFE86PPyt0I5PYUuZasH5LPFJ/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1s93ZCmoAsAIJLmm8FZtfhp_gp2uN88Hy/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
